b11da46eb71d057730244b1182b7f66f6843f0ae
RuslanSalimov <mail-spam2012@yandex.ru>
2021-03-24 02:43:32 +0500

    Исправлены подсказки

--- src/modules/iconHoverInit.js
+++ src/modules/iconHoverInit.js
@@ -3,6 +3,8 @@ const iconHoverInit = () => {
 
     const showIconPopup = (e) => {
         const element = e.target;
+        const parent = element.closest('.formula-item');
+        const formulaItems = document.querySelectorAll('.formula-item');
         let iconPopup = element.querySelector('.formula-item-popup');
         const iconInner = element.querySelector('.formula-item__icon-inner');
         iconInner.style.opacity = '1';
@@ -12,11 +14,16 @@ const iconHoverInit = () => {
             iconPopup.classList.add('formula-item-popup__reverted');
         }
         iconPopup.classList.add('active');
+        formulaItems.forEach(item => {
+            item.style.zIndex = '-1';
+        });
+        parent.style.zIndex = '1';
     }
     const closeIconPopup = (e) => {
         const element = e.target;
         const iconPopup = element.querySelectorAll('.formula-item-popup');
         const iconInner = element.querySelector('.formula-item__icon-inner');
+        const formulaItems = document.querySelectorAll('.formula-item');
         iconInner.style.opacity = '';
         iconPopup.forEach(item => {
             if(item.classList.contains('formula-item-popup__reverted')){
@@ -26,6 +33,9 @@ const iconHoverInit = () => {
                 item.classList.remove('active');
             }
             //item.style.cssText = ``;
+        });
+        formulaItems.forEach(item => {
+            item.style.zIndex = '0';
         });
 
     }
