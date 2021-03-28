cabc42dd9d3ab48cbc94112068e259f216464ec1
RuslanSalimov <mail-spam2012@yandex.ru>
2021-03-28 17:27:41 +0500

    правки

--- src/modules/accordion.js
+++ src/modules/accordion.js
@@ -4,6 +4,11 @@ const accordion = () => {
     accordionElement.addEventListener('click', (e) => {
         let target = e.target.closest('.title_block');
         if(target){
+            itemElements.forEach(item => {
+                if(item.classList.contains('msg-active')){
+                    item.classList.remove('msg-active');
+                }
+            })
             target.classList.toggle('msg-active');
         }
     })
